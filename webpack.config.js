ef29dcd55be22739fb0c635f8293106e9ecd6858
Dmitry Strezh <36566007+dstressed@users.noreply.github.com>
2018-06-20 00:38:50 +0300

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entry: './js/client.js',
+    output: {
+        path: __dirname + '/js',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8000
+    }
+}
